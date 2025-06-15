5ccf7752fe22ff713fb2028250720898584471bd
izidizy <dzarycinski@gmail.com>
2025-06-16 01:25:21 +0200

    three.js, GLTFLoader.js, OrbitControls.js and BufferGeometryUtils.js added, basic scene with howtitzer working

--- libs/GLTFLoader.js
+++ libs/GLTFLoader.js
@@ -64,8 +64,8 @@ import {
 	VectorKeyframeTrack,
 	SRGBColorSpace,
 	InstancedBufferAttribute
-} from 'three';
-import { toTrianglesDrawMode } from '../utils/BufferGeometryUtils.js';
+} from '../libs/three.module.js';
+import { toTrianglesDrawMode } from './BufferGeometryUtils.js';
 
 class GLTFLoader extends Loader {
 
--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import * as THREE from './libs/three.module.js';
-import { GLTFLoader } from './libs/GLTFLoader.js';
-import { OrbitControls } from './libs/OrbitControls.js';
+import { GLTFLoader } from './libs/GLTFLoader.js'
+import { OrbitControls } from './libs/OrbitControls.js'
 
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0xaaaaaa);
